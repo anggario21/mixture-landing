f4bb629b1884ab288f1973e645cd5a1277541b6b
anggario21d <anggario_21@apps.ipb.ac.id>
2023-03-29 01:14:04 +0700

    add carousel

--- script.js
+++ script.js
@@ -30,3 +30,65 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+
+const slides = document.querySelectorAll(".carousel-slide");
+const prevBtn = document.querySelector(".carousel-prev");
+const nextBtn = document.querySelector(".carousel-next");
+
+let index = 0;
+slides[index].classList.add("active");
+
+function moveRight() {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}
+
+function moveLeft() {
+  slides[index].classList.remove("active");
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add("active");
+}
+
+prevBtn.addEventListener("click", moveLeft);
+nextBtn.addEventListener("click", moveRight);
+
+// Get the slider container and navigation buttons
+const sliderContainer = document.querySelector(".slider-container");
+const prevButton = document.querySelector(".slider-prev");
+const nextButton = document.querySelector(".slider-next");
+
+// Get the slider wrapper and individual slides
+const sliderWrapper = document.querySelector(".slider-wrapper");
+const sliderSlides = document.querySelectorAll(".slider-slide");
+
+// Set the initial slide to 0
+let currentSlideIndex = 0;
+
+// Add the active class to the initial slide
+sliderSlides[currentSlideIndex].classList.add("active");
+
+// Set the width of the slider wrapper to fit all slides
+sliderWrapper.style.width = `${sliderSlides.length * 100}%`;
+sliderSlides.forEach((sliderSlide) => {
+  sliderSlide.style.width = `${100 / sliderSlides.length}%`;
+});
+
+// Add event listeners to the navigation buttons
+prevButton.addEventListener("click", () => {
+  sliderSlides[currentSlideIndex].classList.remove("active");
+  currentSlideIndex--;
+  if (currentSlideIndex < 0) {
+    currentSlideIndex = sliderSlides.length - 1;
+  }
+  sliderSlides[currentSlideIndex].classList.add("active");
+});
+
+nextButton.addEventListener("click", () => {
+  sliderSlides[currentSlideIndex].classList.remove("active");
+  currentSlideIndex++;
+  if (currentSlideIndex > sliderSlides.length - 1) {
+    currentSlideIndex = 0;
+  }
+  sliderSlides[currentSlideIndex].classList.add("active");
+});
